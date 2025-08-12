ffde9addec3efdeb91fdade43bd6a9fe1c6c5bdb
syed faruk abdulla <syedfarukabdulla55@gmail.com>
2025-08-12 17:16:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'components/react';
-import Header from 'components/Header';
-import Hero from 'components/Hero';
-import About from 'components/About';
-import FeaturedCarousel from 'components/FeaturedCarousel';
-import Programs from 'components/Programs';
-import Impact from 'components/Impact';
-import Gallery from 'components/Gallery';
-import VolunteerDonate from 'components/VolunteerDonate';
-import Testimonials from 'components/Testimonials';
-import Footer from 'components/Footer';
+import React from './components/react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import FeaturedCarousel from './components/FeaturedCarousel';
+import Programs from './components/Programs';
+import Impact from './components/Impact';
+import Gallery from './components/Gallery';
+import VolunteerDonate from './components/VolunteerDonate';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 
 function App() {
   return (
